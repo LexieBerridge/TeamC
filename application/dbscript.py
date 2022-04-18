6ce0174a56e78344ab89d98bcb1e2a42d02b851e
Linda <lindaallinson123@gmail.com>
2022-04-18 14:00:14 +0100

    added to requirements.txt and some boiler plate code to dbs script

---
+++ application/dbscript.py
@@ -0,0 +1,9 @@
+from application.models.teams import Teams
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sess, sessionmaker
+
+engine = create_engine('mysql+pymysql://root:password@localhost/foodstories', echo=True)
+Session = sessionmaker(bind=engine)
+
+session = Session()

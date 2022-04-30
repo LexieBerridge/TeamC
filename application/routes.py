09ad5464f983c7fd1e2cc5025e5123d7e9e902f8
Linda <lindaallinson123@gmail.com>
2022-04-30 17:49:28 +0100

    added some db script

--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,12 @@
 import json
 from flask import render_template, request, jsonify
 import service
-from application import dbscript
-from application import app
-from application.forms import new_user_form
+from application import app, dbscript
 from application.forms.new_user_form import SignUpForm
+from application.models.food_group import FoodGroup
+from application.models.recipe import Recipe
 from application.models.user_table import UserTable
-import application.templates
+
 
 # SIGNUP PAGE: uses a a python class SignUpForm made in new_user_form (in the forms folder) which is presented
 # to the user via Signup.html (templates) and when filled out correctly shows the page welcome_new_user.html (templates)
@@ -17,18 +17,21 @@ def signup():
     if request.method == 'POST':
         form = SignUpForm(request.form)
         print(form.username.data)
-        username = form.username.data
-        user_name = form.user_name.data
+        input_username = form.username.data
+        input_user_name = form.user_name.data
         # user_age = form.user_age.data
-        password = form.password.data
-        if len(username) == 0 or len(password) == 0 or len(user_name) == 0:
+        input_password = form.password.data
+        if len(input_username) == 0 or len(input_password) == 0 or len(input_user_name) == 0:
             error = "Please supply username, password and the name you like to go by"
+        if UserTable.query.filter_by(username=input_username).first() != None:
+            error = "Username already taken please try again"
+            print(error)
         else:
-            user = UserTable(username=username, user_name=user_name, password=password)
-            service.add_new_user(user)
-            return render_template('welcome_new_user.html', user_name=user_name)
-            # return redirect ( url_for('user'), user=user)
+            users_form_details = UserTable(username=input_username, user_name=input_user_name, password=input_password)
+            service.add_new_user(users_form_details)
+            return render_template('welcome_new_user.html', user_name=input_user_name)
     return render_template('signup.html', form=form, message=error)
+
 
 @app.route("/home")
 @app.route("/")
@@ -37,7 +40,10 @@ def home():
 
 @app.route("/recipes")
 def recipes():
-    return render_template("recipes.html")
+    food_group = FoodGroup.query.filter_by(food_group_id=1).first()
+    recipe_name = food_group.group_name
+    recipe_description = food_group.group_fact
+    return render_template("recipes.html", recipe_name=recipe_name, recipe_description=recipe_description)
 
 @app.route('/user_name')
 def user_name():
--- service.py
+++ service.py
@@ -1,6 +1,12 @@
-from application.models import gender, user_table
 from application import db
+
 
 def add_new_user(user):
     db.session.add(user)
     db.session.commit()
+
+# def show_recipe():
+#     recipe = db.session.query(Recipe).filter_by(recipe_id=1).first()
+#     recipe_name = recipe.recipe_name
+#     recipe_description = recipe.recipe_description
+
